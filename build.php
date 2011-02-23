dcee30de021b8e9a3a4305161e43f077c03d7fc5
bearice <bearice@a668db52-b749-bf9e-ecec-9cd76b57eb07>
2011-02-23 16:36:31 +0000

    git-svn-id: https://learning-android-cn.googlecode.com/svn/trunk@3 a668db52-b749-bf9e-ecec-9cd76b57eb07

---
+++ build.php
@@ -0,0 +1,11 @@
+<?php
+$key = ''; // Post-Commit Authentication Key
+$ignorecommits = array('Edited wiki page through web user interface.');
+$data = file_get_contents("php://input");
+$digest = hash_hmac("md5", $data, $key);
+$hook = $_SERVER["HTTP_GOOGLE_CODE_PROJECT_HOSTING_HOOK_HMAC"];
+if (!$hook || ($hook != $digest)) die "bad google auth!";
+$data = json_decode($data, true);
+if (in_array($data['revisions'][0]['message'], $ignorecommits)) die "ingnored commit";
+echo `svn update && make`
+?>
