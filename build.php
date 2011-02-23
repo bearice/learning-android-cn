6d8199d6b8b0aa379508b6dd613557dbb48c2e0c
bearice <bearice@a668db52-b749-bf9e-ecec-9cd76b57eb07>
2011-02-23 16:45:47 +0000

    bug fix : PHP Parse error:  syntax error, unexpected T_CONSTANT_ENCAPSED_STRING in build.php on line 7
    git-svn-id: https://learning-android-cn.googlecode.com/svn/trunk@4 a668db52-b749-bf9e-ecec-9cd76b57eb07

--- build.php
+++ build.php
@@ -4,8 +4,8 @@ $ignorecommits = array('Edited wiki page through web user interface.');
 $data = file_get_contents("php://input");
 $digest = hash_hmac("md5", $data, $key);
 $hook = $_SERVER["HTTP_GOOGLE_CODE_PROJECT_HOSTING_HOOK_HMAC"];
-if (!$hook || ($hook != $digest)) die "bad google auth!";
+if (!$hook || ($hook != $digest)) die("bad google auth!");
 $data = json_decode($data, true);
-if (in_array($data['revisions'][0]['message'], $ignorecommits)) die "ingnored commit";
+if (in_array($data['revisions'][0]['message'], $ignorecommits)) die("ingnored commit");
 echo `svn update && make`
 ?>
